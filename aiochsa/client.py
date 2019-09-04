1b0dc901c55a15f2cbbb1c95c33556520594cdcc
Denis Otkidach <denis.otkidach@gmail.com>
2019-09-04 20:27:57 +0300

    Switching from regexp hack for inserts to proper clause analysis

--- aiochsa/client.py
+++ aiochsa/client.py
@@ -1,78 +1,66 @@
-import re
+from types import SimpleNamespace
 
 from aiochclient import ChClient
 from clickhouse_sqlalchemy.drivers.http.base import dialect
 from clickhouse_sqlalchemy.drivers.http.escaper import Escaper
 from sqlalchemy.sql import func, ClauseElement
+from sqlalchemy.sql.ddl import DDLElement
 from sqlalchemy.sql.dml import Insert
 
-
-RE_INSERT_VALUES = re.compile(
-    r"\s*((?:INSERT|REPLACE)\s.+\sVALUES?\s*)" +
-        r"(\(\s*(?:%s|%\(.+\)s)\s*(?:,\s*(?:%s|%\(.+\)s)\s*)*\))" +
-        r"(\s*(?:ON DUPLICATE.*)?);?\s*\Z",
-    re.IGNORECASE | re.DOTALL,
-)
 
 _dialect = dialect()
 _escaper = Escaper()
 
 
-def execute_defaults(query, args):
-    # Clickhouse doesn't support updates, so we don't need to honor `onupdate`
-    if not isinstance(query, Insert):
-        return query
+def _execute_clauseelement(elem, multiparams):
+    if multiparams:
+        if len(multiparams) == 1 and isinstance(multiparams[0], dict):
+            multiparams = multiparams[0]
+        if isinstance(elem, Insert):
+            elem = elem.values(multiparams)
+        else:
+            elem = elem.params(multiparams)
+    compiled_sql = elem.compile(
+        dialect=_dialect,
+    )
+    return _execute_context(
+        _dialect,
+        _dialect.execution_ctx_cls._init_compiled,
+        compiled_sql,
+        (),
+        compiled_sql,
+        (),
+    )
 
-    if not args:
-        # query.parameters could be a list in a multi row insert
-        args = query.parameters
-        if not isinstance(query.parameters, list):
-            args = [args]
+def _execute_ddl(ddl, multiparams):
+    compiled = ddl.compile(dialect=_dialect)
+    return _execute_context(
+        _dialect,
+        _dialect.execution_ctx_cls._init_ddl,
+        compiled,
+        None,
+        compiled,
+    )
 
-    for params in args:
-        _execute_default_attr(query, params)
-    return query
 
-def _execute_default_attr(query, params):
-    # XXX Use _process_executesingle_defaults and _process_executemany_defaults
-    # method of ExecutionContext? Or get_insert_default?
-    # Created from dialect.execution_ctx_cls
-    for col in query.table.columns:
-        attr = col.default
-        if attr and col.name not in params:
-            # Clickhouse doesn't support sequences, so we skip them
-            if attr.is_scalar:
-                params[col.name] = attr.arg
-            elif attr.is_callable:
-                params[col.name] = attr.arg({})
-            else: # pragma: no cover
-                assert False, repr(attr)
+def _execute_context(dialect, constructor, statement, parameters, *args):
+    conn = SimpleNamespace(dialect=dialect, _execution_options={})
+    db_api_conn = SimpleNamespace(cursor=lambda: None)
+    context = constructor(dialect, conn, db_api_conn, *args)
+    assert len(context.parameters) == 1
+    return context.statement % _escaper.escape(context.parameters[0])
 
 
 def compile_query(query, args):
     if isinstance(query, str):
-        return query, args
+        assert not args
+        return query
     elif isinstance(query, ClauseElement):
-        query = execute_defaults(query, args)
-        compiled = query.compile(dialect=_dialect)
-        # TODO Analyse source query instead of compiled string
-        m = RE_INSERT_VALUES.match(compiled.string)
-        if m:
-            q_prefix = m.group(1) % ()
-            q_values = m.group(2).rstrip()
-            values_list = [
-                q_values % _escaper.escape(parameters)
-                for parameters in (args or [compiled.params])
-            ]
-            query = '{} {};'.format(q_prefix, ','.join(values_list))
-            return query, []
+        if isinstance(query, DDLElement):
+            query = _execute_ddl(query, args)
         else:
-            params = args[0] if args else compiled.params
-            if params is None:
-                query = compiled.string
-            else:
-                query = compiled.string % _escaper.escape(params)
-            return query, []
+            query = _execute_clauseelement(query, args)
+        return query
     else: # pragma: no cover
         assert False, type(query)
 
@@ -80,8 +68,8 @@ def compile_query(query, args):
 class ChClientSa(ChClient):
 
     async def _execute(self, query: str, *args):
-        query, args = compile_query(query, args)
-        async for rec in super()._execute(query, *args):
+        query = compile_query(query, args)
+        async for rec in super()._execute(query):
             yield rec
 
     def __await__(self):
