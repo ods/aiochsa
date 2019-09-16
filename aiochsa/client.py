dc471eb0e27a3d6a93236ad63f15f19564bce1a5
ods <denis.otkidach@gmail.com>
2019-09-16 16:09:02 +0300

    Copy a modified `ChClient._execute()` into `ChClientSa`

--- aiochsa/client.py
+++ aiochsa/client.py
@@ -1,14 +1,37 @@
-from aiochclient import ChClient
+from typing import AsyncIterable
+
+from aiochclient.client import ChClient, ChClientError, QueryTypes
+from aiochclient.records import Record, RecordsFabric # TODO To be replaced
 
 from .compiler import compile_statement
 
 
 class ChClientSa(ChClient):
 
-    async def _execute(self, statement: str, *args):
-        statement = compile_statement(statement, args)
-        async for rec in super()._execute(statement):
-            yield rec
+    async def _execute(self, statement: str, *args) -> AsyncIterable[Record]:
+        query = compile_statement(statement, args)
+
+        # The rest is a modified copy of `ChClient._execute()`
+        query_type = self.query_type(query)
+
+        if query_type == QueryTypes.FETCH:
+            query += " FORMAT TSVWithNamesAndTypes"
+        data = query.encode()
+
+        async with self._session.post(
+            self.url, params=self.params, data=data
+        ) as resp:  # type: client.ClientResponse
+            if resp.status != 200:
+                # TODO Parse code and display test (and stack trace?):
+                # https://github.com/yandex/ClickHouse/blob/master/dbms/src/Common/Exception.cpp#L261
+                raise ChClientError((await resp.read()).decode(errors='replace'))
+            if query_type == QueryTypes.FETCH:
+                rf = RecordsFabric(
+                    names=await resp.content.readline(),
+                    tps=await resp.content.readline(),
+                )
+                async for line in resp.content:
+                    yield rf.new(line)
 
     def __await__(self):
         # For compartibility with asyncpg (`await pool.acquire(...)`)
