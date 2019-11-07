ae1ce0af6345d3d00b5065734fafa9aed8300ee6
ods <denis.otkidach@gmail.com>
2019-11-07 16:34:50 +0300

    More tests for DBException

--- tests/test_exc.py
+++ tests/test_exc.py
@@ -15,10 +15,13 @@ async def test_exc(conn):
     assert 'Syntax error' in str(exc_info.value)
 
 
-async def test_exc_stacktrace(dsn):
+@pytest.mark.parametrize(
+    'statement', ['ERROR', 'ERROR ' * 1000],
+)
+async def test_exc_stacktrace(dsn, statement):
     async with aiochsa.connect(dsn, stacktrace=1) as conn:
         with pytest.raises(aiochsa.DBException) as exc_info:
-            await conn.execute('ERROR')
+            await conn.execute(statement)
 
     assert exc_info.value.code == error_codes.SYNTAX_ERROR
     assert 'Syntax error' in exc_info.value.display_text
@@ -26,3 +29,4 @@ async def test_exc_stacktrace(dsn):
 
     assert str(error_codes.SYNTAX_ERROR) in str(exc_info.value)
     assert 'Syntax error' in str(exc_info.value)
+    assert len(str(exc_info.value)) < 2000
