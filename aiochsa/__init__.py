18d72802c8794d9998076590026ef3052ec19f2a
Denis Otkidach <denis.otkidach@gmail.com>
2020-01-17 17:16:15 +0300

    Use setuptools_scm; __version__

--- aiochsa/__init__.py
+++ aiochsa/__init__.py
@@ -1,3 +1,10 @@
+from pkg_resources import get_distribution, DistributionNotFound
+
 from .client import Client
 from .exc import DBException
 from .pool import connect, create_pool
+
+try:
+    __version__ = get_distribution(__name__).version
+except DistributionNotFound:
+    pass
--- setup.py
+++ setup.py
@@ -1,9 +1,6 @@
 from setuptools import setup
-import sys
 
-
-needs_pytest = {'pytest', 'test', 'ptr'}.intersection(sys.argv)
 
 setup(
-    setup_requires = ['pytest-runner'] if needs_pytest else [],
+    use_scm_version = True,
 )
